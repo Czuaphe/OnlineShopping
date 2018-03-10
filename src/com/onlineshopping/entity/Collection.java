da481d87c283c17c21182baf0dd91eb6138b49a8
Zhang1011 <1204908396@qq.com>
2018-03-10 09:31:42 +0800

    提交以前写过的

---
+++ src/com/onlineshopping/entity/Collection.java
@@ -0,0 +1,13 @@
+package com.onlineshopping.entity;
+
+public class Collection extends Goods {
+    private int cid;
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+}
